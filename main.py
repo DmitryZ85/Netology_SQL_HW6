58aa4090a5818229e1573406a78d07d1433a07c8
DmitryZ85 <105450520+DmitryZ85@users.noreply.github.com>
2022-10-25 21:02:49 +0300

    Initial commit for the HW6 (ORM)

---
+++ main.py
@@ -0,0 +1,86 @@
+import sqlalchemy
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+import json
+
+Base = declarative_base()
+
+DSN = 'postgresql://postgres:Z25instr@localhost:5432/netology_hw6'
+engine = sqlalchemy.create_engine(DSN)
+con = engine.connect()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class Publisher(Base):
+    __tablename__ = "publisher"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=80), unique=True)
+
+
+class Book(Base):
+    __tablename__ = "book"
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(length=100), nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
+
+    publisher = relationship(Publisher, backref="book")
+
+
+class Shop(Base):
+    __tablename__ = "shop"
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=80), nullable=False)
+
+
+class Stock(Base):
+    __tablename__ = "stock"
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+    count = sq.Column(sq.Integer)
+
+    book = relationship(Book, backref="stock")
+    shop = relationship(Shop, backref="stock")
+
+
+class Sale(Base):
+    __tablename__ = "sale"
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Float, nullable=False)
+    date_sale = sq.Column(sq.Date)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
+    count = sq.Column(sq.Integer)
+
+    stock = relationship(Stock, backref="sale")
+
+
+def create_tables(engine):
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+create_tables(engine)
+
+with open(test_data.json, 'r') as f:
+    data = json.load(f)
+
+for record in data:
+    model = {
+        'publisher': Publisher,
+        'shop': Shop,
+        'book': Book,
+        'stock': Stock,
+        'sale': Sale,
+    }[record.get('model')]
+    session.add(model(id=record.get('pk'), **record.get('fields')))
+session.commit()
+
+res = input('Введите id издателя ')
+
+query_1 = session.query(Publisher).filter(Publisher.id == res)
+query_1
+
+for i in query_1.all():
+    print(f'Имя издателя #{res} - {i.name}')
